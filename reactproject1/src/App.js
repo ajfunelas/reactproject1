ecf1a50234bd2fdd3276c64de2c9cdb2c5809a73
ajfunelas <31107247+ajfunelas@users.noreply.github.com>
2019-10-06 23:00:52 +0800

    Complete Protoype

--- reactproject1/src/App.js
+++ reactproject1/src/App.js
@@ -7,25 +7,50 @@ const App = () => {
 
   const APP_ID = '797ce1b3';
   const APP_KEY = 'bae97fb6d6745d4fd78182ee4f42906d';
+  
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
 
   useEffect(() => {
     getRecipes();
-  }, [] );
+  }, [query] );
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
+    console.log(data.hits);
 
+  }
+
+  const updateSearch = e => {
+      setSearch(e.target.value);
+  }
+
+  const getSearch = e => {
+    e.preventDefault(); // this prevents the page refresh
+    setQuery(search);
+    setSearch(''); // this clears the form after every search
   }
 
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text"/>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button className="search-button" type="submit">Search</button>
       </form>
+      <div className="recipes">
+      {recipes.map(recipe =>(
+        <Recipe 
+        key={recipe.recipe.label}
+        title={recipe.recipe.label} calories={recipe.recipe.calories} 
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
+        />
+      ))}
+      </div>
     </div>
   );
 }
