c7105a5b86b5c92ec2e9789e3498214d91959997
ajfunelas <31107247+ajfunelas@users.noreply.github.com>
2019-10-06 21:21:50 +0800

    Ajax Requests

--- reactproject1/src/App.js
+++ reactproject1/src/App.js
@@ -1,18 +1,24 @@
 import React, {useEffect, useState} from 'react';
+import Recipe from './Recipe';
 import './App.css';
+import { async } from 'q';
 
 const App = () => {
 
   const APP_ID = '797ce1b3';
   const APP_KEY = 'bae97fb6d6745d4fd78182ee4f42906d';
-  
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-  
-const [counter, setCounter] = useState(0);
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    console.log('Effect has been run');
-  });
+    getRecipes();
+  }, [] );
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+
+  }
 
   return (
     <div className="App">
@@ -20,7 +26,6 @@ const [counter, setCounter] = useState(0);
         <input className="search-bar" type="text"/>
         <button className="search-button" type="submit">Search</button>
       </form>
-      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 }
