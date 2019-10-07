5f5430ff10d4d4a28b662e1f734668d83551b8dd
ajfunelas <31107247+ajfunelas@users.noreply.github.com>
2019-10-07 09:42:00 +0800

    setRecipe() // comment

--- reactproject1/src/App.js
+++ reactproject1/src/App.js
@@ -20,8 +20,8 @@ const App = () => {
     const response = await fetch(
       `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
-    setRecipes(data.hits);
-    console.log(data.hits);
+    setRecipes(data.hits); // recipes = data.hits
+    console.log(data.hits); 
 
   }
 
